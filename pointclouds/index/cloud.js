db4f8d45bf0d2c76427edb425fa4a7ad5bc08882
antony <antony.barja8@gmail.com>
2023-07-01 13:19:26 -0500

    up

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 2096574,
+    "boundingBox": {
+        "lx": 684962.06,
+        "ly": 9598131.88,
+        "lz": 11.07,
+        "ux": 686336.2699999991,
+        "uy": 9599506.09,
+        "uz": 1385.2799999990314
+    },
+    "tightBoundingBox": {
+        "lx": 684962.06,
+        "ly": 9598131.88,
+        "lz": 11.07,
+        "ux": 686156.86,
+        "uy": 9599506.09,
+        "uz": 155.32
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 11.901007652282715,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
